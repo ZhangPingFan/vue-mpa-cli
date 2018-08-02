b55e632e6a343bf50ce2d0ddec3573573ab5a812
ZhangPingFan <34783075+ZhangPingFan@users.noreply.github.com>
2018-08-02 13:07:53 +0800

    Add files via upload

---
+++ build/utils.js
@@ -0,0 +1,152 @@
+'use strict'
+const path = require('path')
+const config = require('../config')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const packageConfig = require('../package.json')
+
+exports.assetsPath = function (_path) {
+  const assetsSubDirectory = process.env.NODE_ENV === 'production'
+    ? config.build.assetsSubDirectory
+    : config.dev.assetsSubDirectory
+
+  return path.posix.join(assetsSubDirectory, _path)
+}
+
+exports.cssLoaders = function (options) {
+  options = options || {}
+
+  const cssLoader = {
+    loader: 'css-loader',
+    options: {
+      sourceMap: options.sourceMap
+    }
+  }
+
+  const postcssLoader = {
+    loader: 'postcss-loader',
+    options: {
+      sourceMap: options.sourceMap
+    }
+  }
+
+  // generate loader string to be used with extract text plugin
+  function generateLoaders(loader, loaderOptions) {
+    const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
+
+    if (loader) {
+      loaders.push({
+        loader: loader + '-loader',
+        options: Object.assign({}, loaderOptions, {
+          sourceMap: options.sourceMap
+        })
+      })
+    }
+
+    // Extract CSS when that option is specified
+    // (which is the case during production build)
+    if (options.extract) {
+      return ExtractTextPlugin.extract({
+        use: loaders,
+        fallback: 'vue-style-loader'
+      })
+    } else {
+      return ['vue-style-loader'].concat(loaders)
+    }
+  }
+
+  // https://vue-loader.vuejs.org/en/configurations/extract-css.html
+  return {
+    css: generateLoaders(),
+    postcss: generateLoaders(),
+    less: generateLoaders('less'),
+    sass: generateLoaders('sass', { indentedSyntax: true }),
+    scss: generateLoaders('sass'),
+    stylus: generateLoaders('stylus'),
+    styl: generateLoaders('stylus')
+  }
+}
+
+// Generate loaders for standalone style files (outside of .vue)
+exports.styleLoaders = function (options) {
+  const output = []
+  const loaders = exports.cssLoaders(options)
+
+  for (const extension in loaders) {
+    const loader = loaders[extension]
+    output.push({
+      test: new RegExp('\\.' + extension + '$'),
+      use: loader
+    })
+  }
+
+  return output
+}
+
+exports.createNotifierCallback = () => {
+  const notifier = require('node-notifier')
+
+  return (severity, errors) => {
+    if (severity !== 'error') return
+
+    const error = errors[0]
+    const filename = error.file && error.file.split('!').pop()
+
+    notifier.notify({
+      title: packageConfig.name,
+      message: severity + ': ' + error.name,
+      subtitle: filename || '',
+      icon: path.join(__dirname, 'logo.png')
+    })
+  }
+}
+
+// 页面模板
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// 用于做配置的合并处理
+const merge = require('webpack-merge')
+// glob模块允许使用*等符号匹配文件路径, 例如lib/*.js就是获取lib文件夹下的所有js后缀名的文件
+const glob = require('glob')
+// 取得相应的页面路径，src文件夹下的modules文件夹
+const PAGE_PATH = path.resolve(__dirname, '../src/modules')
+const modulePaths = glob.sync(PAGE_PATH + '/*')
+
+var modules = []
+modulePaths.forEach((modulePath) => {
+  let moduleName = modulePath.substring(modulePath.lastIndexOf('\/') + 1)
+  modules.push(moduleName)
+})
+
+exports.entries = () => {
+  let entry = {};
+  modules.forEach(function (pageName) {
+    entry[pageName] = `./src/modules/${pageName}/main.js`;
+  });
+  return entry;
+}
+
+exports.htmlPlugin = () => {
+  let arr = [];
+  modules.forEach(function (pageName) {
+    let conf = {
+      // 模板来源
+      template: `./src/index.html`,
+      // 文件名称
+      filename: process.env.NODE_ENV === 'production' ? path.resolve(__dirname, '../dist/' + `${pageName}.html`) : `${pageName}.html`,
+      // 页面模板需要加对应的js脚本，如果不加这行则每个页面都会引入所有的js脚本
+      chunks: ['manifest', 'vendor', pageName],
+      inject: true
+    }
+    if (process.env.NODE_ENV === 'production') {
+      conf = merge(conf, {
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeAttributeQuotes: true
+        },
+        chunksSortMode: 'dependency'
+      })
+    }
+    arr.push(new HtmlWebpackPlugin(conf));
+  });
+  return arr;
+}
